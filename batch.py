04ac07e00891220caac3496089accb2398ee4ca6
Marc Berneman <marc.berneman@outlook.com>
2022-07-21 15:30:57 +0300

    added save folder to arguments

--- batch.py
+++ batch.py
@@ -2,13 +2,14 @@ import argparse
 from TwoSpin import TwoSpinSpherical
 import numpy as np
 import json
+from pathlib import Path
 
 
-def main(seed, N, B, T, iterations):
+def main(seed: int, N: int, B: float, T: float, iterations: int, save_folder: Path):
     np.random.seed(seed)
     model = TwoSpinSpherical(N, B, T)
     H_array, m_array = model.flip_many_times(iterations)
-    with open(f'{N}_{B}_{T}.json', 'w') as f:
+    with open(save_folder / f'{N}_{B}_{T}.json', 'w') as f:
         data = dict(m_init=m_array[0], m_final=m_array[-1],
                     H_init=H_array[0], H_final=H_array[-1])
         json.dump(data, f)
@@ -21,6 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('B', type=float)
     parser.add_argument('T', type=float)
     parser.add_argument('iterations', type=int)
+    parser.add_argument('save_folder', type=Path)
 
     args = parser.parse_args()
     main(**args.__dict__)
